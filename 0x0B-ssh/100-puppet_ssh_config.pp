c777b351b6e1cc91a58bd4700234b3d37ba393c3
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-08-27 22:40:45 +0000

    Create 100-puppet_ssh_config.pp
    - Set up client SSH configuration file so that we connect to a server without
      typing a password:
        - SSH client configuration configured to use the private key ~/.ssh/school
        - SSH client configuration configured to refuse to authenticate using
          a password

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# This Puppet manifest configures private key ~/.ssh/school
+file { '/etc/ssh':
+  ensure => directory,
+}
+
+# Configure SSH client to use private key and disable password authentication
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#?\s*IdentityFile\s+.+$',
+}
