91555651ab4a2ffefd3d84a1f63bcf15cf6b261e
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-08-30 19:34:02 +0300

    Update 7-puppet_install_nginx_web_server.pp
    - Re write configurations to accomplish the requirements

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,38 +1,21 @@
 # This Puppet manifest configures Nginx and performs a 301 redirection
 
-package { 'nginx':
-  ensure => installed,
-}
+  package { 'nginx':
+    ensure => installed,
+  }
 
-service { 'nginx':
-  ensure => running,
-  enable => true,
-}
+  file { '/var/www/html/nginx':
+    ensure => 'directory',
+  }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
+  exec { 'display_page':
+    command => '/usr/bin/sudo /bin/echo Hello World! > /var/www/html/nginx/index.nginx-debian.html',
+  }
 
-file { '/etc/nginx/sites-available/default':
-  source => 'puppet:///etc/nginx/nginx.conf',
-  notify => Service['nginx'],
-}
+  exec { 'redirect_config':
+    command => '/usr/bin/sudo /bin/echo "location /redirect_me { return 301 http://innovateweb.tech; }" >> /etc/nginx/sites-available/default',
+  }
 
-file { '/etc/nginx/sites-enabled/default':
-  ensure => 'link',
-  target => '/etc/nginx/sites-available/default',
-  notify => Service['nginx'],
-}
-
-exec { 'disable_default_site':
-  command => 'rm /etc/nginx/sites-enabled/default',
-  onlyif  => 'test -L /etc/nginx/sites-enabled/default',
-  notify  => Service['nginx'],
-}
-
-exec { 'redirect_config':
-  command => '/bin/echo "location /redirect_me { return 301 http://innovateweb.tech; }" >> /etc/nginx/sites-available/default',
-  unless  => '/bin/grep -q "location /redirect_me { return 301 http://innovateweb.tech; }" /etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-  notify  => Service['nginx'],
-}
+  exec { 'server_start':
+    command => '/usr/bin/sudo /usr/sbin/service nginx start',
+  }
