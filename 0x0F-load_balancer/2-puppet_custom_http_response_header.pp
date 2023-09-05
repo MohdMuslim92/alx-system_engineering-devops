6e10ab9ced0e6ad7994259abd5bdd51b9a812468
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-09-05 15:49:10 +0300

    Update 2-puppet_custom_http_response_header.pp
    - Configures a brand new Ubuntu machine to the requirements asked

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,9 @@
 # This Puppet manifest configures Nginx to add a custom HTTP response header
 
-exec { 'update':
-  command  => 'sudo apt-get update',
-  provider => shell,
-}
-
-# Ensure Nginx is installed and running
-package { 'nginx':
-  ensure => installed,
-}
-
-service { 'nginx':
-  ensure => running,
-  enable => true,
-}
-
-# Define the custom HTTP header
-file_line { 'header line':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "	location / {
-  add_header X-Served-By ${hostname};",
-  match  => '^\tlocation / {',
-}
-
-# Reload Nginx to apply changes
-exec { 'restart service':
-  command  => 'sudo service nginx restart',
+exec { 'http header':
+  command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
   provider => shell,
 }
